3ebd08c5175ea2339bc2300a9e049f8692217a29
lifecycles1 <raichev89@gmail.com>
2023-03-19 14:43:47 +0000

    Add project files.

---
+++ SuperHeroAPI/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI.Models;
+
+namespace SuperHeroAPI.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("");
+        }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
